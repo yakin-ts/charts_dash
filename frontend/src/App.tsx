d3a6f35742267b1a51365e59b7e2a2001fea1d03
Yakin T. <yakin16tm@gmail.com>
2025-04-06 23:12:53 +0300

    chore: hook redux with api response on render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import { useDispatch } from "react-redux";
+
+import { getTeam, getACVRange, getAccountIndustry, getCustomerType} from "./services/dataApi";
+import { setAllData } from "./store/slices/dataSlice";
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+       const teamData = await getTeam();
+        const acvRangeData = await getACVRange();
+        const accountIndustryData = await getAccountIndustry();
+        const customerTypeData = await getCustomerType();
+        // Dispatch actions to store the data in Redux store
+        dispatch(setAllData({
+          teams: teamData,
+          acvRange: acvRangeData,
+          accountIndustry: accountIndustryData,
+          customerType: customerTypeData
+        }))
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, [dispatch]);
+
+ 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      
+    </div>
   )
 }
 
---
+++ frontend/src/services/dataApi.ts
@@ -0,0 +1,55 @@
+const API_URL = import.meta.env.VITE_API_BASE_URL; // Ensure the variable starts with VITE_
+
+import axios from "axios";
+
+export const getTeam = async () => {
+    try {
+        if (!API_URL) {
+            throw new Error("API_URL is undefined. Check your environment variables.");
+        }
+        const response = await axios.get(`${API_URL}/api/data/team`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching teams data:", error);
+        throw error;
+    }
+};
+
+export const getACVRange = async () => {
+    try {
+        if (!API_URL) {
+            throw new Error("API_URL is undefined. Check your environment variables.");
+        }
+        const response = await axios.get(`${API_URL}/api/data/acv-range`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching ACV range data:", error);
+        throw error;
+    }
+};
+
+export const getAccountIndustry = async () => {
+    try {
+        if (!API_URL) {
+            throw new Error("API_URL is undefined. Check your environment variables.");
+        }
+        const response = await axios.get(`${API_URL}/api/data/account-industry`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching account industry data:", error);
+        throw error;
+    }
+};
+
+export const getCustomerType = async () => {
+    try {
+        if (!API_URL) {
+            throw new Error("API_URL is undefined. Check your environment variables.");
+        }
+        const response = await axios.get(`${API_URL}/api/data/customer-type`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching customer type data:", error);
+        throw error;
+    }
+};
